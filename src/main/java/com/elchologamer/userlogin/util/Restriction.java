42118d8c13511cfd59332b7dc862c78e1d1ab9a0
ElCholoGamer <josedanielgrayson@gmail.com>
2021-02-12 11:36:12 -0500

    add and implement RestrictionEvent

--- src/main/java/com/elchologamer/userlogin/util/Restriction.java
+++ src/main/java/com/elchologamer/userlogin/util/Restriction.java
@@ -2,6 +2,7 @@ package com.elchologamer.userlogin.util;
 
 import com.elchologamer.userlogin.UserLogin;
 import com.elchologamer.userlogin.api.UserLoginAPI;
+import com.elchologamer.userlogin.api.event.RestrictionEvent;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -9,7 +10,7 @@ import org.bukkit.event.Event;
 import org.bukkit.event.entity.EntityEvent;
 import org.bukkit.event.player.PlayerEvent;
 
-public abstract class Restriction<T extends Event> extends BaseListener {
+public abstract class Restriction<E extends Event> extends BaseListener {
 
     private final UserLogin plugin = UserLogin.getPlugin();
     protected final String configKey;
@@ -18,22 +19,25 @@ public abstract class Restriction<T extends Event> extends BaseListener {
         this.configKey = configKey;
     }
 
-    public boolean shouldRestrict(T e) {
+    public boolean shouldRestrict(E e) {
         boolean enabled = plugin.getConfig().getBoolean("restrictions." + configKey);
         if (!enabled) return false;
 
+        RestrictionEvent<E> event = new RestrictionEvent<>(e);
+
         if (e instanceof PlayerEvent) {
             Player p = ((PlayerEvent) e).getPlayer();
-            return !UserLoginAPI.isLoggedIn(p);
-        }
-
-        if (e instanceof EntityEvent) {
+            event.setCancelled(UserLoginAPI.isLoggedIn(p));
+        } else if (e instanceof EntityEvent) {
             Entity entity = ((EntityEvent) e).getEntity();
-            return !entity.getType().equals(EntityType.PLAYER)
-                    || !UserLoginAPI.isLoggedIn((Player) entity);
+            event.setCancelled(
+                    entity.getType().equals(EntityType.PLAYER)
+                            && UserLoginAPI.isLoggedIn((Player) entity)
+            );
         }
 
-        return true;
+        plugin.getServer().getPluginManager().callEvent(event);
+        return !event.isCancelled();
     }
 
     public UserLogin getPlugin() {
