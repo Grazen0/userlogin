f96098c80c45465baca8dee4732132aae1921dee
ElCholoGamer <josedanielgrayson@gmail.com>
2021-10-11 22:32:50 -0500

    exclude aliases on commands restriction

--- src/main/java/com/elchologamer/userlogin/listener/restriction/CommandRestriction.java
+++ src/main/java/com/elchologamer/userlogin/listener/restriction/CommandRestriction.java
@@ -1,8 +1,13 @@
 package com.elchologamer.userlogin.listener.restriction;
 
 import com.elchologamer.userlogin.ULPlayer;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CommandRestriction extends BaseRestriction<PlayerCommandPreprocessEvent> {
 
@@ -14,10 +19,21 @@ public class CommandRestriction extends BaseRestriction<PlayerCommandPreprocessE
     public void handle(PlayerCommandPreprocessEvent e) {
         if (!shouldRestrict(e)) return;
 
-        String m = e.getMessage().replaceAll("^/", "").toLowerCase();
-        if (!m.startsWith("login") && !m.startsWith("register")) {
-            e.setCancelled(true);
-            ULPlayer.get(e.getPlayer()).sendMessage("messages.commands_disabled");
+        String command = e.getMessage().replaceAll("^/", "").split("\\s+")[0];
+
+        List<String> allowedCommands = new ArrayList<>(Arrays.asList("login", "register"));
+
+        ConfigurationSection section = getPlugin().getConfig().getConfigurationSection("commandAliases");
+        if (section != null) {
+            allowedCommands.addAll(section.getStringList("login"));
+            allowedCommands.addAll(section.getStringList("register"));
         }
+
+        for (String allowedCommand : allowedCommands) {
+            if (allowedCommand.equalsIgnoreCase(command)) return;
+        }
+
+        e.setCancelled(true);
+        ULPlayer.get(e.getPlayer()).sendMessage("messages.commands_disabled");
     }
 }
